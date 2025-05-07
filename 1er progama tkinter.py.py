7a93a84597f9fab2f75b8833db05e20234f400f6
MendozaJorge <jorgemendoza.rgz@gmail.com>
2025-05-06 19:28:07 -0600

    Update 1er progama tkinter.py.py

--- 1er progama tkinter.py.py
+++ 1er progama tkinter.py.py
@@ -3,10 +3,12 @@ def saludar():
     nombre=entrada_nombre.get()
     edad=entrada_edad.get()
     etiqueta_resultado.config(text=f"Hola {nombre}, tu edad es de {edad}")
+    
 
 ventana = tk.Tk() 
 ventana.title("Mi primera app grafica") 
 ventana.geometry("400x200")
+ventana.config(bg="aquamarine")
 
 etiqueta_nombre = tk.Label(ventana, text="Ingresa tu nombre:") 
 etiqueta_nombre.pack()
@@ -24,5 +26,11 @@ boton = tk.Button(ventana, text="Mostrar saludo", command=saludar)
 boton.pack() 
 etiqueta_resultado = tk.Label(ventana, text="") 
 etiqueta_resultado.pack()
+
+etiqueta = tk.Label(ventana, text="")
+etiqueta.pack()
+
+etiqueta = tk.Label (ventana, text="El autor de este programa fue: Jorge Mendoza Rodriguez")
+etiqueta.pack()
 
 ventana.mainloop()
